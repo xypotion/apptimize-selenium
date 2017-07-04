2327a4696ea6e9f9261aad65b878d6d6be3ef7e8
Max W <xypotion@gmail.com>
2017-07-04 11:55:09 -0700

    ...and it fails when I change the assertion. cool.

--- python_org_search.py
+++ python_org_search.py
@@ -8,5 +8,6 @@ elem = driver.find_element_by_name("q")
 elem.clear()
 elem.send_keys("pycon")
 elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
+# assert "No results found." not in driver.page_source
+assert "Event" not in driver.page_source
 driver.close()
