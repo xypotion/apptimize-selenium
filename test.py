bcd65f09b32c1575fd1a18806c93f4a3720af67f
Max W <xypotion@gmail.com>
2017-07-04 11:13:17 -0700

    classes & objects. ok.

--- test.py
+++ test.py
@@ -18,3 +18,18 @@ a = a * b
 
 compareAB(a, b)
     
+##########
+    
+class Thing:
+    def __init__(self, size = "big", color = "blue"):
+        self.size = size
+        self.color = color
+        
+    def describeSelf(self):
+        print("i am " + self.size + " and " + self.color + ".")
+        
+myThing = Thing()
+myThing.describeSelf()
+
+otherThing = Thing("small", "red")
+otherThing.describeSelf()
