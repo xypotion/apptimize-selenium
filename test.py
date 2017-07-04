4da6da810899cfa5bb702d8c25347cfcce095df0
Max W <xypotion@gmail.com>
2017-07-04 11:01:09 -0700

    slightly better basic logic test

--- test.py
+++ test.py
@@ -1,4 +1,20 @@
 print("hello world")
 
+def compareAB(a, b):
+    if b > a:
+        print("b > a")
+    else:
+        print("a > b")
+
 if 1: #no true constant? weird.
     print ("true")
+    
+a = 10
+b = 20
+
+compareAB(a, b)
+
+a = a * b
+
+compareAB(a, b)
+    
