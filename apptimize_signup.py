5e0e6856d6faf892b4ff0d0a89983edf85b634e6
Max W <xypotion@gmail.com>
2017-07-04 17:39:15 -0700

    majority of test script in place

--- apptimize_signup.py
+++ apptimize_signup.py
@@ -11,15 +11,41 @@ import time
 def newEmail(testName):
     return "maxawunderlich+{0}{1}@gmail.com".format(testName, int(time.time()))
 
-print(newEmail("TrialSignup"))
+driver = webdriver.Firefox()
 
-# driver = webdriver.Firefox()
-# driver.get("http://www.python.org")
-# assert "Python" in driver.title
-# elem = driver.find_element_by_name("q")
-# elem.clear()
-# elem.send_keys("pycon")
-# elem.send_keys(Keys.RETURN)
+# go straight to trial signup page, since that's faster than loading apptimize.com first and clicking Sign Up
+driver.get("https://apptimize.com/30-day-trial")
+
+# fill in required fields in signup form
+fname = driver.find_element_by_id("fname")
+fname.clear()
+fname.send_keys("Max")
+
+lname = driver.find_element_by_id("lname")
+lname.clear()
+lname.send_keys("Wunderlich")
+
+email = driver.find_element_by_id("email")
+email.clear()
+email.send_keys(newEmail("TrialSignup"))
+
+company = driver.find_element_by_id("company")
+company.clear()
+company.send_keys("Apptimize Candidate")
+
+password = driver.find_element_by_id("password")
+password.clear()
+password.send_keys("password%d" % int(time.time()))
+
+# click "No" for "Have you purchased a software platform in the last 6 months?"
+purchasedNo = driver.find_element_by_xpath('//input[@name="purchased"][@value="No"]')
+purchasedNo.click()
+
+# click Terms & Conditions checkbox
+eula = driver.find_element_by_id("eula")
+eula.click()
+
 # # assert "No results found." not in driver.page_source
 # assert "Event" not in driver.page_source
+
 # driver.close()
